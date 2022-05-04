18ac352aa04987f31a0d59a161184e83881cb8c7
NavaneethVijay <navaneeth.v@codilar.com>
2022-05-04 15:56:43 +0530

    initial commit

---
+++ src/components/ProductShortDescription/productShortDescription.gql.js
@@ -0,0 +1,15 @@
+import { gql } from '@apollo/client';
+export const GET_PRODUCT_SHORT_DESCRIPTION = gql`
+    query getProductShortDescription($urlKey: String!) {
+        products(filter: { url_key: { eq: $urlKey } }) {
+            items {
+                id
+                uid
+                url_key,
+                short_description {
+                    html
+                }
+            }
+        }
+    }
+`
---
+++ src/webpack/componentOverrideMapping.js
@@ -0,0 +1,8 @@
+/**
+ * Mappings for overwrites
+ * example: [`@magento/venia-ui/lib/components/Main/main.js`]: './lib/components/Main/main.js'
+ */
+module.exports = componentOverride = {
+    [`@magento/peregrine/lib/talons/RootComponents/Product/productDetailFragment.gql.js`]:
+     './src/RootComponents/Product/productDetailFragment.gql.js'
+};
